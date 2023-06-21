e262be28a144da8e676a29dd666596f9f4e559cb
clara-moy <c.m0y@yahoo.com>
2023-06-21 10:47:31 +0200

    add protocol

--- from_pcap_to_json.py
+++ from_pcap_to_json.py
@@ -2,6 +2,7 @@ from scapy.all import *
 import json
 from time import time
 
+
 start = time()
 n_sample = "3"
 print("Extracting data...")
@@ -9,27 +10,36 @@ scapy_cap = rdpcap("data/sample_" + n_sample + ".pcap")
 print("Creating dictionnary...")
 data = {}
 data["paquets"] = []
+
+
 index = 0
 for packet in scapy_cap:
     data["paquets"].append({"src": packet[0].src, "dst": packet[0].dst})
+
     try:
         data["paquets"][index].update({"ip_src": packet[1].psrc})
-        data["paquets"][index].update({"ttl": packet[1].ttl})
-    except (AttributeError, IndexError):
-        data["paquets"][index].update({"ip_src": None})
-        data["paquets"][index].update({"ttl": None})
-    try:
         data["paquets"][index].update({"ip_dst": packet[1].pdst})
     except (AttributeError, IndexError):
+        data["paquets"][index].update({"ip_src": None})
         data["paquets"][index].update({"ip_dst": None})
+
+    try:
+        data["paquets"][index].update({"proto": packet[1].proto})
+    except (AttributeError, IndexError):
+        data["paquets"][index].update({"proto": None})
+
+    try:
+        data["paquets"][index].update({"ttl": packet[1].ttl})
+    except (AttributeError, IndexError):
+        data["paquets"][index].update({"ttl": None})
+
     try:
         data["paquets"][index].update({"port_src": packet[2].sport})
-    except (AttributeError, IndexError):
-        data["paquets"][index].update({"port_src": None})
-    try:
         data["paquets"][index].update({"port_dst": packet[2].dport})
     except (AttributeError, IndexError):
+        data["paquets"][index].update({"port_src": None})
         data["paquets"][index].update({"port_dst": None})
+
     index += 1
 
 
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import json
 import networkx as nx
-import matplotlib.pyplot as plt
-import numpy as np
 from bokeh.io import show
 from bokeh.models import (
-    Range1d,
     Circle,
     MultiLine,
     HoverTool,
@@ -12,11 +9,16 @@ from bokeh.models import (
 from bokeh.plotting import figure, curdoc
 from bokeh.plotting import from_networkx
 from time import time
+import socket
 
 print("Extracting data...")
 start = time()
 with open("data/json_data_3.json") as file:
     data = json.load(file)
+with open("numbers/ports.json") as file:
+    ports_db = json.load(file)
+with open("numbers/ip-protocol-numbers.json") as file:
+    ip_protocols_db = json.load(file)
 
 print("Creating graph...")
 
@@ -26,6 +28,7 @@ list_edges = []
 mac = {}
 ip = {i: [] for i in range(len(data["paquets"]))}
 port = {i: [] for i in range(len(data["paquets"]))}
+service = {i: [] for i in range(len(data["paquets"]))}
 node_color = {i: "magenta" for i in range(len(data["paquets"]))}
 n_pkts = {}
 graph = nx.Graph()
@@ -34,15 +37,16 @@ for packet in data["paquets"]:
     src = packet["src"]
     dst = packet["dst"]
 
+    if src not in list_nodes:
+        list_nodes.append(src)
+    if dst not in list_nodes:
+        list_nodes.append(dst)
+
     src_index = list_nodes.index(src)
     dst_index = list_nodes.index(dst)
 
     mac[src_index] = src
     mac[dst_index] = dst
-    if src not in list_nodes:
-        list_nodes.append(src)
-    if dst not in list_nodes:
-        list_nodes.append(dst)
 
     ip_src = packet["ip_src"]
     ip_dst = packet["ip_dst"]
@@ -53,19 +57,42 @@ for packet in data["paquets"]:
 
     port_src = packet["port_src"]
     port_dst = packet["port_dst"]
+
+    if port_src != None and port_src > 1024:
+        port_src = ">1024"
+    if port_dst != None and port_dst > 1024:
+        port_dst = ">1024"
+
     if port_src not in port[src_index]:
         port[src_index].append(port_src)
     if port_dst not in port[dst_index]:
         port[dst_index].append(port_dst)
 
-    if port_src != None and port_src < 1024:
-        node_color[src_index] = "cyan"
-    else:
-        port_src = ">1024"
-    if port_dst != None and port_dst < 1024:
-        node_color[dst_index] = "cyan"
-    else:
-        port_dst = ">1024"
+    proto_number = packet["proto"]
+    if proto_number != None:
+        proto = ip_protocols_db[str(proto_number)]["keyword"].lower()
+        try:
+            if port_src != ">1024" and port_src != None:
+                service_src = socket.getservbyport(port_src, proto)
+                if service_src not in service[src_index]:
+                    service[src_index].append(service_src)
+            if port_dst != ">1024" and port_dst != None:
+                service_dst = socket.getservbyport(port_dst, proto)
+                if service_dst not in service[dst_index]:
+                    service[dst_index].append(service_dst)
+        except OSError:
+            if (
+                port_src != ">1024"
+                and port_src != None
+                and proto not in service[src_index]
+            ):
+                service[src_index].append(proto)
+            elif (
+                port_dst != ">1024"
+                and port_dst != None
+                and proto not in service[dst_index]
+            ):
+                service[dst_index].append(proto)
 
     if (src_index, dst_index) in n_pkts.keys():
         n_pkts[(src_index, dst_index)] += 1
@@ -76,13 +103,23 @@ for packet in data["paquets"]:
 
     graph.add_edge(src_index, dst_index)
 
+for node in graph:
+    if "http" in service[node] or "https" in service[node]:
+        node_color[node] = "green"
+    elif port[node] == [None]:
+        node_color[node] = "pink"
+    elif ">1024" not in port[node]:
+        node_color[node] = "blue"
+    elif port[node] != [">1024"]:
+        node_color[node] = "cyan"
+
 nx.set_node_attributes(graph, name="MAC", values=mac)
 nx.set_node_attributes(graph, name="IP", values=ip)
 nx.set_node_attributes(graph, name="node color", values=node_color)
 nx.set_node_attributes(graph, name="port", values=port)
+nx.set_node_attributes(graph, name="service", values=service)
 nx.set_edge_attributes(graph, name="n_pkts", values=n_pkts)
 
-# create graph renderer from networkx graph
 graph_renderer = from_networkx(graph, nx.spring_layout, scale=10, center=(0, 0))
 
 print("Adjusting layout...")
@@ -90,11 +127,16 @@ plot = figure(
     tools="pan,wheel_zoom,save,reset",
     active_scroll="wheel_zoom",
     x_range=(-12, 12),
-    y_range=(-10, 10),
+    y_range=(-12, 12),
 )
 
 hover_nodes = HoverTool(
-    tooltips=[("MAC", "@MAC"), ("IP", "@IP"), ("Port", "@port")],
+    tooltips=[
+        ("MAC", "@MAC"),
+        ("IP", "@IP"),
+        ("Port", "@port"),
+        ("service", "@service"),
+    ],
     renderers=[graph_renderer.node_renderer],
 )
 
